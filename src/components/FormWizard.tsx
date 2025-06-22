1270710f6e3526b839511e449b40d39a07d93064
Kaliyani <Kaliyani.Periaperumal@nagarro.com>
2025-06-22 17:25:25 +0530

    Family and Finance Tab added

--- src/components/FormWizard.tsx
+++ src/components/FormWizard.tsx
@@ -4,21 +4,22 @@ import ProgressBar from './ProgressBar';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import PersonalInfo from './PersonalInfo';
-import { nextStep } from '@/redux/formSlice';
+import { nextStep, prevStep } from '@/redux/formSlice';
+import FamilyAndFinance from './FamilyAndFinance';
 
 const FormWizard = () => {
   const step = useSelector((state: RootState) => state.form.step);
   const dispatch = useDispatch();
 
   const handleNext = () => dispatch(nextStep());
-  //const handlePrev = () => dispatch(prevStep());
+  const handlePrev = () => dispatch(prevStep());
 
   return (
     <div className="max-w-2xl mx-auto bg-white p-6 shadow-md rounded-md">
       <ProgressBar step={step} />
       {step === 1 && <PersonalInfo onNext={handleNext} />}
-      {/* {step === 2 && <Step2 onNext={handleNext} onBack={handlePrev} />}
-      {step === 3 && <Step3 onBack={handlePrev} />} */}
+      {step === 2 && <FamilyAndFinance onNext={handleNext} onBack={handlePrev} />}
+      {/* {step === 3 && <Step3 onBack={handlePrev} />} */}
     </div>
   );
 };
